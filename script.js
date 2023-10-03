47eea87f653ce2cd0d5ec8df80268d321d4e54bc
Yura <kovalevichyura@gmail.com>
2023-10-03 21:23:39 +0300

    fix btn language

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const languageChangeLink = document.querySelector(".header__language a");
+const burgerMenuLink = document.querySelector(".icon-menu");
+const menuBody = document.querySelector(".menu__body");
 
+languageChangeLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (menuBody.style.left === "0") {
+        menuBody.style.left = "-200%";
+    }
+});
+
+burgerMenuLink.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (menuBody.style.left === "0") {
+        menuBody.style.left = "-200%";
+    } else {
+        menuBody.style.left = "0";
+    }
+});
+
+menuBody.addEventListener("click", (event) => {
+    menuBody.style.left = "-200%";
+});
+
+menuBody.addEventListener("click", (event) => {
+    event.stopPropagation();
+});
